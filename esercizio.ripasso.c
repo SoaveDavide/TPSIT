092e6f3e7f6f658a0052557c97bd772725c752c0
Davide Soave <121793063+SoaveDavide@users.noreply.github.com>
2024-01-11 10:53:58 +0100

    ripasso in itinere prima versione

---
+++ esercizio.ripasso.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <time.h>
+#define DIM 10000
+int p;
+int main(int argc, char *argv[])
+{
+    int vettore[DIM]; // creazione del vettore
+    int n;            // numero inserito dall'utente
+    if (argc != 2)
+    {
+        printf("Inserimento di parametri sbagliato\n");
+        return 1;
+    }
+    else
+    {
+        n = atoi(argv[1]);
+        // generazione dei numeri casuali
+        srand(time(NULL));
+        for (int i = 0; i < DIM; i++)
+        {
+            int random = rand() % 501;
+            vettore[i] = random;
+            // printf("%d\n", vettore[i]);
+        }
+        p = fork();
+        if (p > 0)
+        {
+
+            p = fork();
+            if (p > 0)
+            {
+                // sono nel padre
+                for (int i = 0; i < 2000; i++)
+                {
+                    if (n == vettore[i])
+                    {
+                        // Scrivo sulla linea di comado sia indice che numero che PID
+                        printf("Sono il processo padre e ho PID = %d. Il numero: %d si trova all'indice:%d\n", getpid(), n, i);
+                    }
+                }
+            }
+            else
+            {
+                // Sono nel primo figlio
+                for (int i = 2000; i < 6000; i++)
+                {
+                    if (n == vettore[i])
+                    {
+                        // Scrivo sulla linea di comado sia indice che numero che PID
+                        printf("Sono il processo figlio e ho PID = %d. Il numero: %d si trova all'indice:%d\n", getpid(), n, i);
+                    }
+                }
+                
+            }
+        }
+        else
+        {
+            // Sono nel secondo figlio
+            for (int i = 6000; i < DIM; i++)
+                {
+                    if (n == vettore[i])
+                    {
+                        // Scrivo sulla linea di comado sia indice che numero che PID
+                        printf("Sono il processo figlio 2 e ho PID = %d. Il numero: %d si trova all'indice:%d\n", getpid(), n, i);
+                    }
+                }
+            
+            
+        }
+        return 0;
+    }
+}
