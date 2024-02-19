a5f573b983626afac77c7ddcd997927eae0c211e
SoaveDavide <davide.soave@iisviolamarchesini.edu.it>
2024-02-19 15:04:12 +0100

    fatte modifiche agli esercizi aggiunti

--- fifo_consumatore.c
+++ fifo_consumatore.c
@@ -1,21 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-
+#include <fcntl.h>
 #include <sys/wait.h>
 #include <sys/types.h>
+#include <sys/stat.h>
 #define TOT 5
 // libreria per aprire la FIFO
 #include <fcntl.h>
 int main(int argc, char *argv[])
 {
     int fd;
+    if (mkfifo("sumo", 0666) < 0)
+    {
+        printf("fifo non creata\n");
+        return 1;
+    }
     int somma;
     int vettore[TOT];
     fd = open("sumo", O_RDONLY);
     if (read(fd, &vettore, sizeof(vettore)) == -1)
     {
         printf("Non sono riuscito a leggere\n");
+        return 2;
     }
     printf("Sono riuscito a leggere dalla FIFO\n");
     for (int i = 0; i < TOT; i++)
@@ -23,4 +30,6 @@ int main(int argc, char *argv[])
         somma += vettore[i];
     }
     printf("La somma risulta: %d\n", somma);
+    close(fd);
+    return 0;
 }
--- fifo_produttore.c
+++ fifo_produttore.c
@@ -1,23 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-
+#include <fcntl.h>
 #include <sys/wait.h>
 #include <sys/types.h>
+#include <sys/stat.h>
 #define TOT 5
 // libreria per aprire la FIFO
-#include <fcntl.h>
 int main(int argc, char *argv[])
 {
     int fd;
+    if (mkfifo("sumo", 0666) == -1)
+    {
+        perror("mkfifo");
+        return 1;
+    }
     int vettore[TOT];
     fd = open("sumo", O_WRONLY);
     for (int i = 0; i < TOT; i++)
     {
         int r = rand() % 100;
-        vettore[i] = &r;
+        vettore[i] = r;
     }
-    
+
     if (write(fd, &vettore, sizeof(vettore)) == -1)
     {
         printf("\nLa scrittura è avvenuta in maniera sbagliata");
