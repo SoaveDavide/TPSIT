283435f6b09324b8a07194f4b5d860a0a6721f4a
Davide Soave <davide.soave@iisviolamarchesini.edu.it>
2024-04-13 16:07:44 +0200

    aggiunto array_thread

---
+++ array_thread.c
@@ -0,0 +1,55 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <pthread.h>
+#define DIM 10
+#define CICLO 5
+
+void *InserimentoPrimaMeta(void *s)
+{
+    int *array = (int *)s;
+    // Esegue operazioni sulla prima metà dell'array
+    for (int i = 0; i < CICLO; i++)
+    {
+        if (array[i] == 8)
+        {
+            printf("Il numero 8 si trova in posizione: %d\n", i);
+        }
+    }
+    return NULL;
+}
+
+void *InserimentoSecondaMeta(void *s)
+{
+    int *array = (int *)s;
+    // Esegue operazioni sulla seconda metà dell'array
+    for (int i = 0; i < CICLO; i++)
+    {
+        if (array[i] == 8)
+        {
+            printf("Il numero 8 si trova in posizione: %d\n", i + CICLO);
+        }
+    }
+    return NULL;
+}
+
+int main()
+{
+    int array[DIM];
+    pthread_t threadPrima, threadSeconda;
+    
+    // Fase di inserimento
+    printf("Inserire %d numeri:\n", DIM);
+    for (int i = 0; i < DIM; i++)
+    {
+        scanf("%d", &array[i]);
+    }
+    
+    pthread_create(&threadPrima, NULL, &InserimentoPrimaMeta, (void *)array);
+    pthread_create(&threadSeconda, NULL, &InserimentoSecondaMeta, (void *)(array + CICLO)); // Passaggio dell'indirizzo del sesto elemento
+
+    pthread_join(threadPrima, NULL);
+    pthread_join(threadSeconda, NULL);
+
+    return 0;
+}
