531a093a43c7c13d039a7681ec2338a57900f85e
Davide Soave <davide.soave@iisviolamarchesini.edu.it>
2024-05-06 15:22:04 +0200

    modificato copia.c

--- thread/copia.c
+++ thread/copia.c
@@ -2,31 +2,31 @@
 #include <string.h>
 #include <stdio.h>
 #include <pthread.h>
-#define  BUFFER_DIM 1024
+#define BUFFER_DIM 1024
+
 unsigned char buffer[BUFFER_DIM];
 FILE *sorgente, *destinazione;
+
 void *Lettura(void *s)
 {
-    //continuo a leggere fino a quando ci sono dati all'interno del file
-    while (fread(buffer, 1, sizeof(buffer), sorgente) > 0)
+    size_t bytesRead;
+    while ((bytesRead = fread(buffer, 1, sizeof(buffer), sorgente)) > 0)
     { 
-        pthread_t scrittura;
-        pthread_create(&scrittura, NULL, Scrittura, NULL);
-        pthread_join(scrittura, NULL);
+        
     }
-    
     return NULL;
 }
 
 void *Scrittura(void *s)
 {
-    fwrite(buffer, 1, sizeof(buffer), destinazione);
+    size_t bytesToWrite = (size_t)s;
+    fwrite(buffer, 1, bytesToWrite, destinazione);
     return NULL;
 }
 
 int main(int argc, char *argv[])
 {
-    pthread_t lettura;
+    pthread_t lettura, scrittura;
 
     if (argc != 3)
     {
@@ -34,14 +34,14 @@ int main(int argc, char *argv[])
         return 0;
     }
 
-    sorgente = fopen(argv[1], "r");
+    sorgente = fopen(argv[1], "rb");
     if (sorgente == NULL)
     {
         printf("File sorgente non aperto correttamente\n");
         return 0;
     }
 
-    destinazione = fopen(argv[2], "w");
+    destinazione = fopen(argv[2], "wb");
     if (destinazione == NULL)
     {
         printf("File destinazione non aperto correttamente\n");
@@ -50,8 +50,11 @@ int main(int argc, char *argv[])
     }
 
     pthread_create(&lettura, NULL, Lettura, NULL);
-    pthread_join(lettura, NULL);
+    pthread_create(&scrittura, NULL, Scrittura, NULL);
 
+    pthread_join(lettura, NULL); // Aspetta che il thread di lettura finisca prima di chiudere i file
+    pthread_join(scrittura, NULL); // Aspetta che il thread di scrittura finisca prima di continuare la lettura
+    
     fclose(sorgente);
     fclose(destinazione);
 
