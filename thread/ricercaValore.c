07e724403ffa03d49d29c9487361ca9950bb446e
Davide Soave <davide.soave@iisviolamarchesini.edu.it>
2024-05-06 15:29:10 +0200

    terminato esercizio ricerca valore

--- thread/ricercaValore.c
+++ thread/ricercaValore.c
@@ -10,48 +10,74 @@ int num;
 
 void *RicercaPrima(void *s)
 {
-    for (int i = 0; i < DIM/4; i++)
+    int presente = 0;
+    for (int i = 0; i < DIM / 4; i++)
     {
         if (array[i] == num)
         {
             printf("Il numero %d si trova in posizione: %d\n", num, i);
+            presente++;
         }
+    }
+    if (presente == 0)
+    {
+        printf("Numero non presente nella prima metà\n");
     }
     return NULL;
 }
 
 void *RicercaSeconda(void *s)
 {
-    for (int i = DIM/4; i < DIM/2; i++)
+    int presente = 0;
+    for (int i = DIM / 4; i < DIM / 2; i++)
     {
         if (array[i] == num)
         {
+            presente++;
             printf("Il numero %d si trova in posizione: %d\n", num, i);
         }
+    }
+    if (presente == 0)
+    {
+        printf("Numero non presente nella seconda metà\n");
     }
     return NULL;
 }
 
 void *RicercaTerza(void *s)
 {
-    for (int i = DIM/2; i < (DIM/2+DIM/4); i++)
+    int presente = 0;
+    for (int i = DIM / 2; i < (DIM / 2 + DIM / 4); i++)
     {
         if (array[i] == num)
         {
             printf("Il numero %d si trova in posizione: %d\n", num, i);
+            presente++;
         }
+        
+    }
+    if (presente == 0)
+    {
+        printf("Numero non presente nella terza metà\n");
     }
     return NULL;
 }
 
 void *RicercaQuarta(void *s)
 {
-    for (int i = (DIM/2+DIM/4); i < DIM; i++)
+    int presente = 0;
+    for (int i = (DIM / 2 + DIM / 4); i < DIM; i++)
     {
         if (array[i] == num)
         {
             printf("Il numero %d si trova in posizione: %d\n", num, i);
+            presente ++;
         }
+        
+    }
+    if (presente == 0)
+    {
+        printf("Numero non presente nella quarta metà\n");
     }
     return NULL;
 }
@@ -59,7 +85,7 @@ void *RicercaQuarta(void *s)
 int main(int argc, char *argv[])
 {
     pthread_t threadPrima, threadSeconda, threadTerza, threadQuarta;
-    
+
     // faccio inserire all'utente il numero che si vuole ricercare
     printf("Inserire numero da ricercare: \n");
     scanf("%d", &num);
@@ -80,6 +106,6 @@ int main(int argc, char *argv[])
     pthread_join(threadSeconda, NULL);
     pthread_join(threadTerza, NULL);
     pthread_join(threadQuarta, NULL);
-    
+
     return 0;
 }
