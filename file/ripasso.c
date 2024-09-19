c9feae2877e5bc26584c0f33f714af5761382a39
Davide Soave <davide.soave@iisviolamarchesini.edu.it>
2024-09-19 13:37:52 +0200

    nel file ripasso aggiunti alcuni punti

--- file/ripasso.c
+++ file/ripasso.c
@@ -2,79 +2,125 @@
 #include <stdlib.h>
 #include <math.h>
 #include <time.h>
-#define DIM 100
+#define DIM 5
 
 void Inserimento(int vettore[DIM])
 {
     // Generazione di numeri casuali all'interno dell'array
-    for(int i = 0; i < DIM; i++)
+    for (int i = 0; i < DIM; i++)
     {
         vettore[i] = rand() % 101; // Numeri casuali tra 0 e 100
     }
 }
 void Stampa(int vettore[DIM])
 {
-    for(int i = 0; i < DIM; i++)
+    for (int i = 0; i < DIM; i++)
     {
         printf("%d\n", vettore[i]);
     }
 }
+void VisualizzaDispari(int vettore[DIM])
+{
+    for (int i = 0; i < DIM; i++)
+    {
+        if (vettore[i] % 2 != 0)
+        {
+            printf("%d\n", vettore[i]);
+        }
+    }
+}
+void VisualizzaPari(int vettore[DIM])
+{
+    for (int i = 0; i < DIM; i++)
+    {
+        if (vettore[i] % 2 == 0)
+        {
+            printf("%d\n", vettore[i]);
+        }
+    }
+}
+void Invertito(int vettore[DIM])
+{
+    for (int i = DIM; i >= 0; i--)
+    {
+        printf("%d\n", vettore[i]);
+    }
+}
+void Ricerca(int ricerca, int vettore[DIM])
+{
+    for (int i = 0; i < DIM; i++)
+    {
+        if (ricerca == vettore[i])
+        {
+            printf("Il numero ricercato è: %d\n", vettore[i]);
+        }
+    }
+}
 void Menu()
 {
-    printf("[1] Visualizzare a video gli elementi dell’array\n");    
-    printf("[2] Visualizzare a video l’array invertito\n");    
-    printf("[3] Visualizzare a video la somma e la media degli elementi del vettore\n");    
-    printf("[4] Visualizzare a video tutti i numeri pari\n");    
-    printf("[5] Visualizzare a video tutti i numeri dispari\n");    
-    printf("[6] Ricercare un numero (letto in input) nell’array\n");    
-    printf("[7] Eliminare un elemento (letto in input) dell’array\n");    
-    printf("[8] Alternare (o scambiare) a due a due le posizioni del vettore\n");    
-    printf("[9] Ordinare il vettore\n");    
-    printf("[10] Uscire\n");   
+    printf("[1] Visualizzare a video gli elementi dell’array\n");
+    printf("[2] Visualizzare a video l’array invertito\n");
+    printf("[3] Visualizzare a video la somma e la media degli elementi del vettore\n");
+    printf("[4] Visualizzare a video tutti i numeri pari\n");
+    printf("[5] Visualizzare a video tutti i numeri dispari\n");
+    printf("[6] Ricercare un numero (letto in input) nell’array\n");
+    printf("[7] Eliminare un elemento (letto in input) dell’array\n");
+    printf("[8] Alternare (o scambiare) a due a due le posizioni del vettore\n");
+    printf("[9] Ordinare il vettore\n");
+    printf("[10] Uscire\n");
 }
-int main()
+int main(int argc, char *argv[])
 {
     int vettore[DIM];
     srand(time(NULL)); // Inizializzazione del seme per la generazione casuale
     Inserimento(vettore);
-    int uscire = 0;//variabile che mi permette di uscire dal ciclo
-    do 
+    int uscire = 0; // variabile che mi permette di uscire dal ciclo
+    do
     {
         Menu();
-        int scelta; //scelta nel menu
+        int scelta; // scelta nel menu
         printf("Inserisci scelta:\n");
         scanf("%d", &scelta);
-    switch(scelta)
-    {
+        switch (scelta)
+        {
         case 1:
-            //Visualizzazione dell'array
+            // Visualizzazione dell'array
             printf("I numeri dell'array sono: \n");
             Stampa(vettore);
-        break;
+            break;
         case 2:
-        break;
+            printf("Array invertito:\n");
+            Invertito(vettore);
+            break;
         case 3:
-        break;
+            break;
         case 4:
-        break;
+            printf("I numeri pari sono: \n");
+            VisualizzaPari(vettore);
+            break;
         case 5:
-        break;
+            printf("I numeri dispari sono: \n");
+            VisualizzaDispari(vettore);
+            break;
         case 6:
-        break;
+            int ricerca;
+            printf("Inserisci il numero da ricercare:\n");
+            scanf("%d", &ricerca);
+            Ricerca(ricerca, vettore);
+            break;
         case 7:
-        break;
+            break;
         case 8:
-        break;
+            break;
         case 9:
-        break;
+            break;
         case 10:
-        uscire = 1;
-        break;
-        default: 
-        printf("Scelta non valida\n");
-        break;
-        
-    }
-    } while(uscire == 0);
+            uscire = 1;
+            break;
+        default:
+            printf("Scelta non valida\n");
+            break;
+        }
+    } while (uscire == 0);
     return 0;
 }
