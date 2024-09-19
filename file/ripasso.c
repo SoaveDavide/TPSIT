a98d8ac84cf142bdace51c17c5bffff76189b178
Davide Soave <121793063+SoaveDavide@users.noreply.github.com>
2024-09-19 08:53:01 +0200

    Update ripasso.c

--- file/ripasso.c
+++ file/ripasso.c
@@ -6,33 +6,38 @@
 
 void Inserimento(int vettore[DIM])
 {
-    //generazione di numeri casuali all'interno dell'array
-    for(int i=0; i < DIM; i++)
+    // Generazione di numeri casuali all'interno dell'array
+    for(int i = 0; i < DIM; i++)
     {
-        vettore[i] = rand()%101;
+        vettore[i] = rand() % 101; // Numeri casuali tra 0 e 100
     }
 }
+
 void Menu()
 {
-    printf("[1] Visualizzi a video gli elementi dell’array\n");    
-    printf("[2] Visualizzi a video l’array invertito\n");    
-    printf("[3] Visualizzi a video la somma e media degli elementi del vettore\n");    
-    printf("[4] Visualizzi a video tutti i numeri pari\n");    
-    printf("[5] Visualizzi a video tutti i numeri dispari\n");    
-    printf("[6] Ricerchi un numero (letto in input) nell’array\n");    
-    printf("[7] Elimini un elemento (letto in input) dell’array\n");    
-    printf("[8] Alterni (o scambi ) a due a due le posizioni del vettore\n");    
-    printf("[9] Ordini il vettore\n");    
+    printf("[1] Visualizzare a video gli elementi dell’array\n");    
+    printf("[2] Visualizzare a video l’array invertito\n");    
+    printf("[3] Visualizzare a video la somma e la media degli elementi del vettore\n");    
+    printf("[4] Visualizzare a video tutti i numeri pari\n");    
+    printf("[5] Visualizzare a video tutti i numeri dispari\n");    
+    printf("[6] Ricercare un numero (letto in input) nell’array\n");    
+    printf("[7] Eliminare un elemento (letto in input) dell’array\n");    
+    printf("[8] Alternare (o scambiare) a due a due le posizioni del vettore\n");    
+    printf("[9] Ordinare il vettore\n");    
     printf("[10] Uscire\n");   
 }
 int main()
 {
     int vettore[DIM];
+    srand(time(NULL)); // Inizializzazione del seme per la generazione casuale
+    Inserimento(vettore);
     int uscire = 0;//variabile che mi permette di uscire dal ciclo
     do 
     {
+        Menu();
         int scelta; //scelta nel menu
-        scanf("%")
+        printf("Inserisci scelta:\n");
+        scanf("%d", &scelta);
     switch(scelta)
     {
         case 1:
@@ -56,10 +61,11 @@ int main()
         case 10:
         uscire = 1;
         break;
+        default: 
+        printf("Scelta non valida\n");
+        break;
         
     }
-    } while(uscire == 0)
-    
-    
+    } while(uscire == 0);
     return 0;
 }
